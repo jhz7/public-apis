2f2ce1397aef9b4f4a7fe1ef70ae9ea68bb57e09
jhz7 <jhzambrano7@gmail.com>
2019-08-15 16:29:56 -0500

    inicio exposición apis externas

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
-const fs = require('fs');
 const express = require('express');
 const app = express();
  
-app.get('/', function (req, res) {
-  res.json('Hello World')
-})
+app.get('/colombian-festival-days', function (_, res) {
+  const festiveDays = require('../assets/colombian-festival-days.json')
+  res.json(festiveDays)
+});
+
+app.get('/increase-percentage-by-kind-of-worked-hour', function (req, res) {
+  const festiveDays = require('../assets/increase-percentage-by-kind-of-worked-hour.json')
+  res.json(festiveDays)
+});
+
+app.get('/colombian-start-end-nocturnal-schedule', function (req, res) {
+  const festiveDays = require('../assets/start-end-nocturnal-schedule.json')
+  res.json(festiveDays)
+});
  
 app.listen(3000, () => {
     console.log('Escuchando puerto 3000')
